df6f9d8bdf64844b668f149aa2a16bf66cd7cd8f
r0man0v-r0man <romanovmailbox@mail.ru>
2019-04-26 00:08:14 +0300

    Add project files.

---
+++ RNews/DAL/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNews.DAL
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+        public User User { get; set; }
+        public Post()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
---
+++ RNews/DAL/dbContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNews.DAL.dbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public DbSet<User> People { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
