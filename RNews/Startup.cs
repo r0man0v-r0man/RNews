2f60e2e79f1c15c307606ca68f220df57989f18f
Roman <romanovmailbox@mail.ru>
2019-04-29 16:00:12 +0300

    add google authenthication

--- RNews/Startup.cs
+++ RNews/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -27,26 +28,37 @@ namespace RNews
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+
+            //});
+            services.AddAuthentication(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-                
-            });
-            services.AddAuthentication().AddCookie(options =>
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+
+                .AddCookie(options =>
             {
                 options.LoginPath = "/auth/login";
+                options.LogoutPath = "/auth/logout";
                 options.AccessDeniedPath = "/auth/accessdenied";
             })
-                    
+                .AddGoogle(options =>
+                 {
+                     options.ClientId = "992356344313-782hen42mjrmheel8415uoe63r2tgsek.apps.googleusercontent.com";
+                     options.ClientSecret = "I6o49F54Ms4CRuGutcG7LOR7";
+                     
+                 })
             ;
             services.AddDbContext<ApplicationDbContext>(options => options
                     .UseSqlServer(Configuration.GetConnectionString("RNewsDatabase")));
             services.AddIdentity<User, IdentityRole>()
                     .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,7 +74,7 @@ namespace RNews
             }
 
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
             app.UseAuthentication();
             app.UseMvc(routes =>
             {
