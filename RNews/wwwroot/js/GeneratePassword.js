f8c260d0abc23f287b9fef0b2810bf3db78eb593
Roman <romanovmailbox@mail.ru>
2019-05-01 22:36:46 +0300

    add async await to generate password js

--- RNews/wwwroot/js/GeneratePassword.js
+++ RNews/wwwroot/js/GeneratePassword.js
@@ -1,19 +1,20 @@
 ﻿"use strict";
-var connection = new signalR.HubConnectionBuilder().withUrl("/GeneratePasswordHub").build();
 
+(async function () {
+    var connection = new signalR.HubConnectionBuilder().withUrl("/GeneratePasswordHub").build();
 
-connection.on("sendPassword", function (randomPassword) {
-    document.getElementById("auto-password").innerHTML = "Generated password:&nbsp;&nbsp;&nbsp;" + "<kbd>" + randomPassword + "</kbd>";
-});
-
-connection.start()
-    .then(function () {
-        console.log("connection started");
-        document.getElementById("generate-password").onclick = function () {
-            connection.invoke("send");
-        }
-        
-    })
-    .catch(error => {
-        console.error(error.message);
+    connection.on("sendPassword", function (randomPassword) {
+        document.getElementById("auto-password").innerHTML = "Generated password:&nbsp;&nbsp;&nbsp;" + "<kbd>" + randomPassword + "</kbd>";
     });
+
+    await connection.start()
+        .then(function () {
+            console.log("connection started");
+            document.getElementById("generate-password").onclick = function () {
+            connection.invoke("send");
+            }
+        })
+        .catch(error => {
+            console.error(error.message);
+        });
+})();
