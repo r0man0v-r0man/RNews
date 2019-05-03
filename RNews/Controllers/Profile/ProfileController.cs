b97ce2461fb873504043948e7524663b35554f9c
Roman <romanovmailbox@mail.ru>
2019-05-03 15:05:04 +0300

    truying to add pattern layer

--- RNews/Controllers/Profile/ProfileController.cs
+++ RNews/Controllers/Profile/ProfileController.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RNews.DAL;
+using RNews.Models.ViewModels;
 
 namespace RNews.Controllers.Profile
 {
     public class ProfileController : Controller
     {
-        [Route("~/Profile")]
-        public IActionResult Profile()
+        private UserManager<User> userManager { get; }
+        public ProfileController(UserManager<User> userManager)
         {
-            return View();
+            this.userManager = userManager;
+        }
+        
+        [Route("~/Profile")]
+        public IActionResult Profile(ProfileViewModel model)
+        {
+            var userId = userManager.GetUserId(HttpContext.User);
+            var user = userManager.FindByIdAsync(userId).Result;
+            model.Name = user.UserName;
+            model.Email = user.Email;
+            return View(model);
         }
     }
 }
---
+++ RNews/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNews.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
