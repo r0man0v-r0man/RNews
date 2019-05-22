d18b9cbd0a573fa2302c1b39641ca604959a7653
r0man0v r0man <romanovmailbox@mail.ru>
2019-05-22 09:46:11 +0300

    search by titile post done

--- RNews/Controllers/Search/SearchController.cs
+++ RNews/Controllers/Search/SearchController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RNews.DAL.dbContext;
+using RNews.Models.ViewModels;
 
 namespace RNews.Controllers.Search
 {
     public class SearchController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext db;
+        public SearchController(ApplicationDbContext db)
         {
-            return View();
+            this.db = db;
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var posts = from p in db.Posts select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                posts = posts.Where(s => s.Title.Contains(searchString));
+            }
+            var searchResult = await posts.ToListAsync();
+            var searchModelList = new List<SearchViewModel>();
+            foreach (var post in searchResult)
+            {
+                searchModelList.Add(new SearchViewModel
+                {
+                    Id = post.PostId,
+                    Title = post.Title,
+                    Author = post.User.UserName,
+                    Description = post.Description,
+                    DateOfCreated = post.Created.ToShortDateString()
+                });
+            }
+            return View(searchModelList);
         }
     }
 }
---
+++ RNews/Models/ViewModels/SearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNews.Models.ViewModels
+{
+    public class SearchViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string  DateOfCreated { get; set; }
+        public string Author { get; set; }
+    }
+}
