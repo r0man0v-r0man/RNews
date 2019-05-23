e54093d478a0060584c7464c91280982a9f80e1c
r0man0v r0man <romanovmailbox@mail.ru>
2019-05-23 15:58:10 +0300

    fix js

--- RNews/wwwroot/js/UserProperty.js
+++ RNews/wwwroot/js/UserProperty.js
@@ -108,25 +108,25 @@ $(document).ready(function () {
         }
     });
 });
-function doCheck() {
-    var allFilled = true;
-    var inputs = document.getElementsByTagName('input');
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == "text" && inputs[i].value == "")  {
-            allFilled = false;
-            break;
-        }
-    }
+//function doCheck() {
+//    var allFilled = true;
+//    var inputs = document.getElementsByTagName('input');
+//    for (var i = 0; i < inputs.length; i++) {
+//        if (inputs[i].type == "text" && inputs[i].value == "")  {
+//            allFilled = false;
+//            break;
+//        }
+//    }
 
-    document.getElementById("user-property-btn").disabled = !allFilled;
-}
+//    document.getElementById("user-property-btn").disabled = !allFilled;
+//}
 
-window.onload = function () {
-    var inputs = document.getElementsByTagName('input');
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == "text") {
-            inputs[i].onkeyup = doCheck;
-            inputs[i].onblur = doCheck;
-        }
-    }
-};
+//window.onload = function () {
+//    var inputs = document.getElementsByTagName('input');
+//    for (var i = 0; i < inputs.length; i++) {
+//        if (inputs[i].type == "text") {
+//            inputs[i].onkeyup = doCheck;
+//            inputs[i].onblur = doCheck;
+//        }
+//    }
+//};
