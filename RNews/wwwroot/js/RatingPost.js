ea00364e9a215f195e98299af64bfe78d48edbae
Roman <romanovmailbox@mail.ru>
2019-06-07 01:27:22 +0300

    change star rating

--- RNews/wwwroot/js/RatingPost.js
+++ RNews/wwwroot/js/RatingPost.js
@@ -1,6 +1,18 @@
-﻿$(document).ready(function () {
-    $(".rating").rating();
-    $("#star-post").click(rateMyPost)
+﻿$(".my-rating").starRating({
+    totalStars: 5,
+    emptyColor: 'lightgray',
+    hoverColor: 'orange',
+    activeColor: 'gold',
+    initialRating: 4,
+    strokeWidth: 0,
+    useGradient: false,
+    starSize: 25,
+    ratedColor: 'crimson',
+    useFullStars: true,
+    callback: function (currentRating, $el) {
+        rateMyPost(currentRating);
+        console.log('DOM element ', $el);
+    }
 });
 "use strict";
 var connectionRating = new signalR.HubConnectionBuilder().withUrl("/RatingPostHub").build();
@@ -8,7 +20,7 @@ var userId = document.getElementById("user-id").value;
 var postId = document.getElementById("post-id").value;
 
 connectionRating.on("RecieveRating", function (allRating) {
-    document.getElementById("all-rating").innerText = "Рейтинг: " + allRating;
+    //document.getElementById("all-rating").innerText = "Рейтинг: " + allRating;
     console.log(allRating);
 });
 
@@ -19,9 +31,9 @@ connectionRating.start()
     .catch(error => {
         console.error(error.message);
     });
-function rateMyPost() {
-    var ratingPost = document.getElementById("post-rating").value;
-    connectionRating.invoke("Rating", ratingPost, postId, userId)
+function rateMyPost(currentRating) {
+    //var ratingPost = document.getElementById("post-rating").value;
+    connectionRating.invoke("Rating", currentRating, postId, userId)
         .catch(function (err) {
             return console.error(err.toString());
         });
